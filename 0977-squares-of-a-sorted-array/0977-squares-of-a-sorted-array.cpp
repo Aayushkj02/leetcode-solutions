b5adfd8114faea99ac62a826b6cfde16dd9628c0
Aayush Joshi <aayushq82@gmail.com>
2026-07-16 22:04:42 +0530

    Time: 9 ms (19.6%), Space: 30.3 MB (39.83%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,12 +1,24 @@
+//This is the optimal approach with time complexity: O(n) rather than my previous approach with time complexity: O(n (log n))
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> ans;
-        for(int i=0; i<nums.size(); i++){
-            int sq = nums[i] * nums[i];
-            ans.push_back(sq);
+        int n = nums.size();
+        vector<int> ans(n);
+        int i=0;
+        int j=n-1;
+        int index=n-1;
+
+        while(i<=j){
+            if(abs(nums[i]) > abs(nums[j])){
+                ans[index] = nums[i] * nums[i];
+                i++;
+            }
+            else{
+                ans[index] = nums[j] * nums[j];
+                j--;
+            }
+            index--;
         }
-        sort(ans.begin(), ans.end());
         return ans;
     }
 };
