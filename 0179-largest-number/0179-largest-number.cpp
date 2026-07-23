498d469e955523885fd968e369f81c5ae629c3b0
Aayush Joshi <aayushq82@gmail.com>
2026-07-23 19:16:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), compare);
+        if(nums[0] == 0){
+            return "0";
+        }
+
+        string res = " ";
+        for(int num : nums){
+            res = res + to_string(num);
+        }
+        return res;
+    }
+
+    static bool compare(int a, int b){
+        string s1 = to_string(a);
+        string s2 = to_string(b);
+        return s1 + s2 > s2 + s1;
+    }
+};
