9a436e266bb73a4f7eb6d66669dd544576ac55a1
Aayush Joshi <aayushq82@gmail.com>
2026-07-25 13:11:14 +0530

    Time: 0 ms (100%), Space: 9.9 MB (5.88%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int maxProduct(int n) {
+        
         string s = to_string(n);
-
-        int maximum = 0;
-
-        for (int i = 0; i < s.size(); i++) {
-            for (int j = i + 1; j < s.size(); j++) {
-                int a = s[i] - '0';
-                int b = s[j] - '0';
-                maximum = max(maximum, a * b);
-            }
-        }
-
-        return maximum;
+        sort(s.rbegin() , s.rend());
+        int maxProduct = (s[0]-'0')*(s[1]-'0');
+        return maxProduct;
     }
 };
