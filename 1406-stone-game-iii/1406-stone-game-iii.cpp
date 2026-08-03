33d6070c785c8f24ae77d11a568fab03f726e6ed
Aayush Joshi <aayushq82@gmail.com>
2026-08-03 22:42:50 +0530

    Time: 15 ms (82.12%), Space: 136.1 MB (76.04%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,28 +2,29 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        vector<int> scoreDiff(n+1, 0); //Max diff between Alice & Bob
-        scoreDiff[n] = 0;
+        vector<int> scoreDiff (n+1, 0);
 
-        for(int i=n-1; i>=0; i--){
-            scoreDiff[i] = INT_MIN;
+        for (int i = n - 1; i >= 0; i--){
             int sum = 0;
+            int best = INT_MIN;
 
-            for(int j = 0; j<3 && i+j<n; j++){
-                sum = sum + stoneValue[i+j];
-                scoreDiff[i] = max(scoreDiff[i], sum - scoreDiff[i+j+1]);
+            for (int j = 0; j < 3 && j + i < n; j++){
+                sum += stoneValue[i+j];
+                best = max(best, sum - scoreDiff[i+j+1]);
             }
+
+            scoreDiff[i] = best;
+
         }
-        
-        if(scoreDiff[0] > 0){
+        if (scoreDiff[0] > 0) {
             return "Alice";
         }
 
-        else if(scoreDiff[0] < 0){
+        else if (scoreDiff[0] < 0) {
             return "Bob";
         }
-
-        else{
+        
+        else {
             return "Tie";
         }
     }
