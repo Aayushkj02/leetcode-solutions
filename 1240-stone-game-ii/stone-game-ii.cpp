47ec4bb533636b3e59f8a2dbac2d08bc69ef4b89
Aayush Joshi <aayushq82@gmail.com>
2026-08-09 23:02:47 +0530

    Time: 11 ms (54.67%) | Memory: 13.7 MB (55.90%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<int> sum (n+1, 0);
+        for(int i = n-1; i >= 0; i--){
+            sum[i] = sum[i+1] + piles[i];
+        }
+
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int i = n-1; i >= 0; i--){
+            for(int m = 1; m <= n; m++){
+                if(2*m >= n - i)
+                    dp[i][m] = sum[i];
+                else{
+                    for(int x = 1; x <= 2 * m; x++){
+                        dp[i][m] = max(dp[i][m], sum[i] - dp[i + x][max(m, x)]);
+                    }
+                }
+            }
+        }
+        return dp[0][1];
+    }
+};
